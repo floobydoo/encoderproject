dc192f106c72b113d3ae83927e9551c8be04d99a
Matthew <matthewbrittain3d@gmail.com>
2016-10-21 09:11:02 -0700

    Added Decoder
    Fixed errors.

--- encoder.js
+++ encoder.js
@@ -1,111 +1,159 @@
 /* KEYSTROKE TO ALPHABETICAL CHARACTERS */
 var keyString = {
-    32: " ",
-    65: "a",
-    66: "b",
-    67: "c",
-    68: "d",
-    69: "e",
-    70: "f",
-    71: "g",
-    72: "h",
-    73: "i",
-    74: "j",
-    75: "k",
-    76: "l",
-    77: "m",
-    78: "n",
-    79: "o",
-    80: "p",
-    81: "q",
-    82: "r",
-    83: "s",
-    84: "t",
-    85: "u",
-    86: "v",
-    87: "w",
-    88: "x",
-    89: "y",
-    90: "z"
-  }
+  32: " ",
+  65: "a",
+  66: "b",
+  67: "c",
+  68: "d",
+  69: "e",
+  70: "f",
+  71: "g",
+  72: "h",
+  73: "i",
+  74: "j",
+  75: "k",
+  76: "l",
+  77: "m",
+  78: "n",
+  79: "o",
+  80: "p",
+  81: "q",
+  82: "r",
+  83: "s",
+  84: "t",
+  85: "u",
+  86: "v",
+  87: "w",
+  88: "x",
+  89: "y",
+  90: "z"
+}
 /* CHARACTER SHIFT - ONE DOWN */
-  var casesarShift = {
-    "a": "z",
-    "b": "a",
-    "c": "b",
-    "d": "c",
-    "e": "d",
-    "f": "e",
-    "g": "f",
-    "h": "g",
-    "i": "h",
-    "j": "i",
-    "k": "j",
-    "l": "k",
-    "m": "l",
-    "n": "m",
-    "o": "n",
-    "p": "o",
-    "q": "p",
-    "r": "q",
-    "s": "r",
-    "t": "s",
-    "u": "t",
-    "v": "u",
-    "w": "v",
-    "x": "w",
-    "y": "x",
-    "z": "y",
-    " ": " "
-  }
+var casesarShift = {
+  "a": "z",
+  "b": "a",
+  "c": "b",
+  "d": "c",
+  "e": "d",
+  "f": "e",
+  "g": "f",
+  "h": "g",
+  "i": "h",
+  "j": "i",
+  "k": "j",
+  "l": "k",
+  "m": "l",
+  "n": "m",
+  "o": "n",
+  "p": "o",
+  "q": "p",
+  "r": "q",
+  "s": "r",
+  "t": "s",
+  "u": "t",
+  "v": "u",
+  "w": "v",
+  "x": "w",
+  "y": "x",
+  "z": "y",
+  " ": " "
+}
+var reverseShift = {
+  "a": "b",
+  "b": "c",
+  "c": "d",
+  "d": "e",
+  "e": "f",
+  "f": "g",
+  "g": "h",
+  "h": "i",
+  "i": "j",
+  "j": "k",
+  "k": "l",
+  "l": "m",
+  "m": "n",
+  "n": "o",
+  "o": "p",
+  "p": "q",
+  "q": "r",
+  "r": "s",
+  "s": "t",
+  "t": "u",
+  "u": "v",
+  "v": "w",
+  "w": "x",
+  "x": "y",
+  "y": "z",
+  "z": "a",
+  " ": " "
+}
 /* LETTERS TO HEIROGLYPHICS - DISPLAYS IMAGES */
-  var hConverter = {
-    "a": "<img src='images/heiroglyphics/a.gif'>",
-    "b": "<img src='images/heiroglyphics/b.gif'>",
-    "c": "<img src='images/heiroglyphics/c.gif'>",
-    "d": "<img src='images/heiroglyphics/d.gif'>",
-    "e": "<img src='images/heiroglyphics/e.gif'>",
-    "f": "<img src='images/heiroglyphics/f.gif'>",
-    "g": "<img src='images/heiroglyphics/g.gif'>",
-    "h": "<img src='images/heiroglyphics/h.gif'>",
-    "i": "<img src='images/heiroglyphics/i.gif'>",
-    "j": "<img src='images/heiroglyphics/j.gif'>",
-    "k": "<img src='images/heiroglyphics/k.gif'>",
-    "l": "<img src='images/heiroglyphics/l.gif'>",
-    "m": "<img src='images/heiroglyphics/m.gif'>",
-    "n": "<img src='images/heiroglyphics/n.gif'>",
-    "o": "<img src='images/heiroglyphics/o.gif'>",
-    "p": "<img src='images/heiroglyphics/p.gif'>",
-    "q": "<img src='images/heiroglyphics/q.gif'>",
-    "r": "<img src='images/heiroglyphics/r.gif'>",
-    "s": "<img src='images/heiroglyphics/s.gif'>",
-    "t": "<img src='images/heiroglyphics/t.gif'>",
-    "u": "<img src='images/heiroglyphics/u.gif'>",
-    "v": "<img src='images/heiroglyphics/v.gif'>",
-    "w": "<img src='images/heiroglyphics/w.gif'>",
-    "x": "<img src='images/heiroglyphics/x.gif'>",
-    "y": "<img src='images/heiroglyphics/y.gif'>",
-    "z": "<img src='images/heiroglyphics/z.gif'>"
-  }
+var hConverter = {
+  "a": "<img src='images/heiroglyphics/a.gif'>",
+  "b": "<img src='images/heiroglyphics/b.gif'>",
+  "c": "<img src='images/heiroglyphics/c.gif'>",
+  "d": "<img src='images/heiroglyphics/d.gif'>",
+  "e": "<img src='images/heiroglyphics/e.gif'>",
+  "f": "<img src='images/heiroglyphics/f.gif'>",
+  "g": "<img src='images/heiroglyphics/g.gif'>",
+  "h": "<img src='images/heiroglyphics/h.gif'>",
+  "i": "<img src='images/heiroglyphics/i.gif'>",
+  "j": "<img src='images/heiroglyphics/j.gif'>",
+  "k": "<img src='images/heiroglyphics/k.gif'>",
+  "l": "<img src='images/heiroglyphics/l.gif'>",
+  "m": "<img src='images/heiroglyphics/m.gif'>",
+  "n": "<img src='images/heiroglyphics/n.gif'>",
+  "o": "<img src='images/heiroglyphics/o.gif'>",
+  "p": "<img src='images/heiroglyphics/p.gif'>",
+  "q": "<img src='images/heiroglyphics/q.gif'>",
+  "r": "<img src='images/heiroglyphics/r.gif'>",
+  "s": "<img src='images/heiroglyphics/s.gif'>",
+  "t": "<img src='images/heiroglyphics/t.gif'>",
+  "u": "<img src='images/heiroglyphics/u.gif'>",
+  "v": "<img src='images/heiroglyphics/v.gif'>",
+  "w": "<img src='images/heiroglyphics/w.gif'>",
+  "x": "<img src='images/heiroglyphics/x.gif'>",
+  "y": "<img src='images/heiroglyphics/y.gif'>",
+  "z": "<img src='images/heiroglyphics/z.gif'>"
+}
 /*Checking for radio buttons */
-  $("input[type='radio']").click(function(){
-    $("input:checked").prop('checked', false);
-    $(this).prop('checked', true);
-    document.getElementById("inputText").value = "";
-    document.getElementById("textArea").textContent = "";
-    /*Checking for radio type*/
-  });
+$("input[type='radio']").click(function(){
+  $("input:checked").prop('checked', false);
+  $(this).prop('checked', true);
+  document.getElementById("inputText").value = "";
+  document.getElementById("textArea").textContent = "";
+  /*Checking for radio type*/
+});
 
-  $("#inputText").keydown(function(e){
-    var keystroke = e.keyCode;
-    console.log(keyString[keystroke]);
-    var ggez = keyString[keystroke];
-    /*Displaying converted items*/
-    if($("input:checked").val() == "echo"){
-      $("#textArea").append(keyString[keystroke]);
-    } else if($("input:checked").val() == "caesar"){
-      $("#textArea").append(casesarShift[ggez]);
-    } else if($("input:checked").val() == "heiroglyphics"){
-      $("#textArea").append(hConverter[ggez]);
-    }
-  });
+$("#inputText").keydown(function(e){
+  var keystroke = e.keyCode;
+  console.log(keyString[keystroke]);
+  var ggez = keyString[keystroke];
+  /*Displaying converted items*/
+  if($("input:checked").val() == "echo"){
+    $("#textArea").append(keyString[keystroke]);
+  }
+  else if($("input:checked").val() == "caesar"){
+    $("#textArea").append(casesarShift[ggez]);
+  }
+  else if($("input:checked").val() == "heiroglyphics"){
+    $("#textArea").append(hConverter[ggez]);
+  }
+});
+
+/*$('#translationInputText').keydown(function(e){
+if(e.keyCode == 13){
+var message = $('#translationInputText').val();
+var messageArray = message.split("");
+for(i=0; i <= messageArray.length; i++){
+var translateNum = messageArray[i].charCodeAt(0)-32+1;
+$('#translationArea').append(message[translateNum]);
+}
+}
+});*/
+
+$('#translationInputText').keydown(function(e){
+  var keystroke = e.keyCode;
+  var ggez = keyString[keystroke];
+  $("#translationArea").append(reverseShift[ggez]);
+});
